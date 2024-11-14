ab86206736b72bbf392346facd90131c536924d1
basava1139 <106962015+basava1139@users.noreply.github.com>
2024-11-14 21:14:56 +0530

    Update gencsv.sh

--- gencsv.sh
+++ gencsv.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if two arguments are provided
 if [ "$#" -ne 2 ]; then
   echo "Usage: ./gencsv.sh <start_index> <end_index>"
   exit 1
@@ -10,7 +9,6 @@ start=$1
 end=$2
 output="inputFile"
 
-# Generate the file with index and random numbers
 > $output
 for (( i=$start; i<=$end; i++ )); do
   echo "$i, $(( RANDOM % 1000 ))" >> $output
